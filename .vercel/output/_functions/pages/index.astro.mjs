eda74fbbb6f007247c353711132fc833c6412d7f
Jose Carlos Gonzalez <jgonzalez@flexor.mx>
2026-01-13 16:02:27 -0600

    ....

--- .vercel/output/_functions/pages/index.astro.mjs
+++ .vercel/output/_functions/pages/index.astro.mjs
@@ -4,7 +4,7 @@ import 'piccolore';
 export { renderers } from '../renderers.mjs';
 
 const $$Index = createComponent(($$result, $$props, $$slots) => {
-  return renderTemplate`<html lang="es"> <head><meta charset="utf-8"><link rel="icon" type="image/jpeg" sizes="32x32" href="https://uzgnfwbztoizcctyfdiv.supabase.co/storage/v1/object/public/Imagenes/AZuAXHqalTLlz8th7NMdBA-AZuAXHqaHD92HliWBxJzdA.jpg"><title>AdLocal</title>${renderHead()}</head> <body> ${renderComponent($$result, "BusinessWraper", null, { "client:only": "react", "client:component-hydration": "only", "client:component-path": "C:/Users/USER/source/repos/AdLocalWeb/src/wrappers/BusinessWraper", "client:component-export": "default" })} </body></html>`;
+  return renderTemplate`<html lang="es"> <head><meta charset="utf-8"><link rel="icon" type="image/jpeg" sizes="32x32" href="https://uzgnfwbztoizcctyfdiv.supabase.co/storage/v1/object/public/Imagenes/AZuAXHqalTLlz8th7NMdBA-AZuAXHqaHD92HliWBxJzdA.jpg">${renderHead()}</head> <body> <!-- El componente real solo en cliente --> ${renderComponent($$result, "BusinessWraper", null, { "client:only": "react", "client:component-hydration": "only", "client:component-path": "C:/Users/USER/source/repos/AdLocalWeb/src/wrappers/BusinessWraper", "client:component-export": "default" })} </body></html>`;
 }, "C:/Users/USER/source/repos/AdLocalWeb/src/pages/index.astro", void 0);
 
 const $$file = "C:/Users/USER/source/repos/AdLocalWeb/src/pages/index.astro";
--- src/wrappers/BusinessWraper.tsx
+++ src/wrappers/BusinessWraper.tsx
@@ -1,14 +1,13 @@
 import React from "react";
 import App from "../components/App";
-import Comercio from "../components/business/Comercio";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "../styles/global.css";
 import "../styles/leaflet.css";
 import BusinessTabsWrapper from "./BusinessTabsWrapper";
 
-interface BusinessWraperProps {}
-
-const BusinessWraper: React.FC<BusinessWraperProps> = () => {
+const BusinessWraper: React.FC<{ children: React.ReactNode }> = ({
+  children,
+}) => {
   return (
     <App>
       <div>
