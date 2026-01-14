8dacc5ac21ebb946d52180b78d78ba754555a1d4
Jose Carlos Gonzalez <jgonzalez@flexor.mx>
2026-01-13 19:37:51 -0600

    ...

--- src/components/business/MapaComercio.client.tsx
+++ src/components/business/MapaComercio.client.tsx
@@ -12,7 +12,18 @@ interface Props {
 
 const MapaComercio: React.FC<Props> = ({ lat, lng }) => {
   return (
-    <MapContainer center={[lat, lng]} zoom={16} style={{ height: 300, width: "100%" }}>
+    <MapContainer
+      center={[lat, lng]}
+      zoom={16}
+      style={{ height: 300, width: "100%" }}
+      scrollWheelZoom={false}
+      zoomControl={false}
+      dragging={false}
+      doubleClickZoom={false}
+      touchZoom={false}
+      keyboard={false}
+      boxZoom={false}
+    >
       <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
       <Marker position={[lat, lng]} />
     </MapContainer>
