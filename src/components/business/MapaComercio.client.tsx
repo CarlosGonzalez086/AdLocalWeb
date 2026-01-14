7be197200449134975da6f267ac6f9d0ab966f3b
Jose Carlos Gonzalez <jgonzalez@flexor.mx>
2026-01-13 19:48:03 -0600

    ...

--- src/components/business/MapaComercio.client.tsx
+++ src/components/business/MapaComercio.client.tsx
@@ -3,12 +3,27 @@
 
 import React from "react";
 import { MapContainer, TileLayer, Marker } from "react-leaflet";
+import L from "leaflet";
 import "leaflet/dist/leaflet.css";
 
 interface Props {
   lat: number;
   lng: number;
 }
+
+const materialIcon = new L.DivIcon({
+  html: `
+    <span class="material-symbols-outlined" style="
+      font-variation-settings: 'FILL' 0, 'wght' 400, 'GRAD' 0, 'opsz' 24;
+      font-size: 36px;
+      color: #5B3A29;
+      text-shadow: 0 0 2px #000;
+    ">location_on</span>
+  `,
+  className: "",
+  iconSize: [36, 36],
+  iconAnchor: [18, 36],
+});
 
 const MapaComercio: React.FC<Props> = ({ lat, lng }) => {
   return (
@@ -25,7 +40,7 @@ const MapaComercio: React.FC<Props> = ({ lat, lng }) => {
       boxZoom={false}
     >
       <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-      <Marker position={[lat, lng]} />
+      <Marker position={[lat, lng]} icon={materialIcon} />
     </MapContainer>
   );
 };
