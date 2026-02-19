ae125eeba0dd76790dda243109465631a079dc46
Jose Carlos Gonzalez <jgonzalez@flexor.mx>
2026-02-18 18:47:42 -0600

    ...

--- .vercel/output/_functions/entry.mjs
+++ .vercel/output/_functions/entry.mjs
@@ -1,6 +1,6 @@
 import { renderers } from './renderers.mjs';
 import { c as createExports, s as serverEntrypointModule } from './chunks/_@astrojs-ssr-adapter_DLU-WsXs.mjs';
-import { manifest } from './manifest_Cjxcy6MV.mjs';
+import { manifest } from './manifest_ztsDYcNi.mjs';
 
 const serverIslandMap = new Map();;
 
@@ -25,7 +25,7 @@ const _manifest = Object.assign(manifest, {
     middleware: () => import('./_noop-middleware.mjs')
 });
 const _args = {
-    "middlewareSecret": "9c174a96-dd12-4b7f-8b11-86c2ea288b7e",
+    "middlewareSecret": "63f4910a-97fd-4408-9c19-01e103fa5152",
     "skewProtection": false
 };
 const _exports = createExports(_manifest, _args);
