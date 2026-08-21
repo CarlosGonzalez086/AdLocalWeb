b2dcbca72392939d9800fcbadc6f88afc0d1a89b
Jose Carlos Gonzalez <jgonzalez@flexor.mx>
2026-08-21 13:51:01 -0600

    ...

--- src/api/http.ts
+++ src/api/http.ts
@@ -1,7 +1,7 @@
 export const BACKEND_URL =
   import.meta.env.MODE === "development"
-    ? import.meta.env.PUBLIC_BACKEND_URL
-    : import.meta.env.PUBLIC_BACKEND_PROD_URL;
+    ? "http://localhost:8080/api/"
+    : "https://adlocalapi.onrender.com/api/";
 
 export const URL_ADLOCAL_COMERCIO =
   import.meta.env.MODE === "development"
